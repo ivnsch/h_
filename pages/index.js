087a29038e4d08534402c86fa711961fd11eec7a
Ivan Schuetz <ivanschuetz@gmail.com>
2024-11-04 18:30:14 +0100

    Remove not used files

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,17 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { useEffect, useRef, useState } from "react";
-import { App } from "./control/app";
+import { Renderer } from "./renderer";
 
 export default function Home() {
   const [gpuSupported, setGpuSupported] = useState(false);
   const canvasRef = useRef(null);
 
-  const [keyText, setKeyText] = useState("");
-  const [mouseXLabel, setMouseXLabel] = useState("");
-  const [mouseYLabel, setMouseYLabel] = useState("");
-
   useEffect(() => {
-    setGpuSupported(!!navigator.gpu);
     const nested = async () => {
-      const app = new App(
-        canvasRef.current,
-        setKeyText,
-        setMouseXLabel,
-        setMouseYLabel,
-        document
-      );
-      await app.Initialize();
-      app.run();
+      const renderer = new Renderer(canvasRef.current);
+      await renderer.Initialize();
+      renderer.render();
     };
     nested();
   }, []);
@@ -42,18 +31,7 @@ export default function Home() {
             <div>Gpu is not supported!</div>
           )} */}
 
-          <h2>Current Key: {keyText}</h2>
-          <h2 id="key-label"></h2>
-          <h2>Mouse X: {mouseXLabel}</h2>
-          <h2 id="mouse-x-label"></h2>
-          <h2>Mouse Y: {mouseYLabel}</h2>
-          <h2 id="mouse-y-label"></h2>
-          <canvas
-            id="gfx-main"
-            width="800"
-            height="600"
-            ref={canvasRef}
-          ></canvas>
+          <canvas width="800" height="600" ref={canvasRef}></canvas>
         </div>
       </main>
     </div>
